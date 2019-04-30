4238e28b5a2a111ea0b804e01192a857b257be93
samigor <47845766+samigor@users.noreply.github.com>
2019-04-30 14:50:48 +0300

    bootstrap

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,8 @@
+@extends ('layout')
+@section('content')
+<div class="container">
+<h1> Header </h1>
+<p> Post</p>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function(){
+
+Route::get('/','PostController@index');
+Route::get('/posts/{post}','PostController@show');
+
+
+
+/*Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function(){
   Route::get('/', 'DashboardController@dashboard')->name('admin.index');
-});
-Route::get('/', function () {
+});*/
+/*Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/page', function () {
     return view('page');
 });
 Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+Route::get('/tasks/{task}', 'TasksController@show');*/
 /*Route::get('/tasks', function ()
 {
 //$tasks = DB::table('tasks') ->get();
@@ -39,6 +45,6 @@ return view('tasks.show', compact('task'));
   }
 );
 */
-Auth::routes();
+/*Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');*/
