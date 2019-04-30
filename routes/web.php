ab04e3ea07b1d3c5237b7fdfe1007db420fc2c6f
samigor <47845766+samigor@users.noreply.github.com>
2019-04-30 12:30:34 +0300

    tasks

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function(){
+  Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/page', function () {
+    return view('page');
+});
+
+Route::get('/tasks', function ()
+{
+$tasks = DB::table('tasks') ->get();
+return view('tasks.index', compact('tasks'));
+  }
+);
+
+Route::get('/tasks/{task}', function ($id)
+{
+$task = DB::table('tasks') ->find($id);
+//dd ($task);
+return view('tasks.show', compact('task'));
+  }
+);
 
 Auth::routes();
 
